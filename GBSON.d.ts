99d2e6b667a7db0c78ff7448b9549a2d3d2bc740
Pascal Lehwark <pascal@ntzwrk.de>
2019-10-22 08:34:59 +0200

    initial commit after renaming

---
+++ GBSON.d.ts
@@ -0,0 +1,152 @@
+
+export type GBSON = {
+	meta: {
+		format: {
+			name: "GBSON",
+			version: string,
+			url: "https://github.com/lehwark/GBSON/blob/master/GBSON.d.ts"
+		},
+		circular: boolean,
+		locus: string,
+		length: number,
+		datetime: DateTime,
+		references?: Reference[],
+		definition?: string,
+		accession?: string,
+		version?: string,
+		keywords?: string,
+		source?: string,
+		organism?: string,
+		dblink?: string
+	},
+	features: Feature[],
+	origin: string
+}
+
+
+export type Reference = {
+	title: string,
+	index?: number,
+	range?: Range,
+	journal?: string,
+	pubmed?: string,
+	remark?: string,
+	consrtm?: string,
+	authors?: string
+}
+
+export type Range = [number, number] | { complement: Range } | { join: Range[] };
+
+export type Feature = {
+	id: string,
+	type: FeatureType,
+	range: Range,
+	gene?: string,
+	translation?: string,
+	features?: Feature[],
+	acronym?: string,
+	allele?: string,
+	altitude?: string,
+	anticodon?: string,
+	authority?: string,
+	bio_material?: string,
+	biotype?: string,
+	breed?: string,
+	cell_line?: string,
+	cell_type?: string,
+	chromosome?: string,
+	citation?: string,
+	clone?: string,
+	clone_lib?: string,
+	codon_recognized?: string,
+	codon_start?: string,
+	collected_by?: string,
+	collection_date?: string,
+	common?: string,
+	country?: string,
+	cultivar?: string,
+	culture_collection?: string,
+	db_xref?: string,
+	dev_stage?: string,
+	direction?: string,
+	ec_number?: string,
+	ecotype?: string,
+	estimated_length?: string,
+	exception?: string,
+	experiment?: string,
+	function?: string,
+	gap_type?: string,
+	gene_synonym?: string,
+	genotype?: string,
+	group?: string,
+	haplogroup?: string,
+	haplotype?: string,
+	host?: string,
+	host$?: string,
+	identified_by?: string,
+	inference?: string,
+	isolate?: string,
+	isolation_source?: string,
+	lab_host?: string,
+	lat_lon?: string,
+	linkage_evidence?: string,
+	locus_tag?: string,
+	map?: string,
+	mating_type?: string,
+	mobile_element_type?: string,
+	mol_type?: string,
+	ncrna_class?: string,
+	no_qualifier?: string,
+	nomenclature?: string,
+	not_allowed?: string,
+	note?: string,
+	number?: string,
+	operon?: string,
+	organelle?: string,
+	organism?: string,
+	pcr_primers?: string,
+	plastid?: string,
+	pop_variant?: string,
+	product?: string,
+	protein_id?: string,
+	pseudo?: string,
+	pseudogene?: string,
+	regulatory_class?: string,
+	replace?: string,
+	ribosomal_slippage?: string,
+	rpt_family?: string,
+	rpt_type?: string,
+	rpt_unit_range?: string,
+	rpt_unit_seq?: string,
+	satellite?: string,
+	specimen_voucher?: string,
+	standard_name?: string,
+	strain?: string,
+	sub_species?: string,
+	sub_strain?: string,
+	synonym?: string,
+	tag_peptide?: string,
+	tissue_lib?: string,
+	tissue_type?: string,
+	trans_splicing?: string,
+	transcript_id?: string,
+	transl_except?: string,
+	transl_table?: string,
+	translationinput?: string,
+	variety?: string
+}
+
+export type FeatureType = "3'utr" | "5'utr" | "assembly_gap" | "cds" | "conflict" | "d-loop" | "exon" | "gap" | "gene"
+	| "intron" | "misc_difference" | "misc_feature" | "misc_rna" | "misc_structure" | "mobile_element" | "mrna"
+	| "ncrna" | "no_feature" | "non_sense" | "operon" | "orna" | "prim_transcript" | "regulatory" | "rep_origin"
+	| "repeat_region" | "rrna" | "stem_loop" | "sts" | "telomere" | "tmrna" | "trna" | "variation";
+
+
+export type DateTime = {
+	year: number,
+	month: number,
+	day: number,
+	hour: number,
+	minute: number,
+	second?: number
+}
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # GBSON
+File format definition used by [the GeSeq annotation software](https://chlorobox.mpimp-golm.mpg.de/geseq.html) to represent GenBank data as JSON.
+
+## Example JSON output
+An example output can be found here: [GBSON-Example.json](https://chlorobox.mpimp-golm.mpg.de/GBSON-Example.json).
+
+## Usage 
+To use GBSON in your application you can use any standard JSON parser. If you are programming in JavaScript, just do
+```javascript
+const data = JSON.parse(gbson);
+```
+
+In TypeScript you should add the type declaration from this repository to get full IDE support 
+```typescript
+import { GBSON } from "./GBSON";
+
+const data:GBSON = JSON.parse(gbson);
+```
+
+And access the data like this
+```typescript
+const gene = data.features[0].gene;
+```
+
+## Range definitions
+
+### GenBank
+```genbank
+complement(join(125294..125832,126877..127429))
+```
+
+### GBSON
+```json
+{ "complement" : { "join" : [ [ 125294, 125832 ], [ 126877, 127429 ] ] } }
+```
+
+## Nested Features
+An advantage of using JSON over GenBank or GFF is its intrinsic capability to express nested structures:
+
+![Nested Features](https://chlorobox.mpimp-golm.mpg.de/GBSON-Nested-Features.jpg "Nested Features")
